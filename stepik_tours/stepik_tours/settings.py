87b0764f15a38fb2d30489351e78a86243a7ed42
Tkachmaxim <tkachmaxim23@gmail.com>
2021-05-21 10:24:54 +0300

    add_static_root

--- stepik_tours/stepik_tours/settings.py
+++ stepik_tours/stepik_tours/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-84&8jdz!vzwi1&5_3rprm5%!#u5q4gbpyvvitsy&zha-eeoy8#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -120,6 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
